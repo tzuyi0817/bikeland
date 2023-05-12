ec051ab7673a32a4db8ae259d2f93f8e04694994
tzuyi <a0988426059@gmail.com>
2023-05-12 09:58:55 +0800

    feat: bilke map component

---
+++ components/common/BikeMap.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import 'leaflet/dist/leaflet.css';
+import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet';
+
+const zoom = ref(17);
+</script>
+
+<template>
+  <div class="fixed w-full h-full top-8">
+    <l-map
+      ref="map"
+      v-model:zoom="zoom"
+      :center="[47.41322, -1.219482]"
+      :min-zoom="2"
+      :useGlobalLeaflet="false"
+    >
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      />
+    </l-map>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Navigation from '@/components/common/Navigation.vue';
 import BikeInfo from '@/components/common/BikeInfo.vue';
+import BikeMap from '@/components/common/BikeMap.vue';
 
 </script>
 
 <template>
   <main>
+    <bike-map />
     <navigation />
     <slot></slot>
     <bike-info />
